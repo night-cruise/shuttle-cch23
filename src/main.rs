c0820cb7deb60ee47b54a6eae5bc757e33d04e57
Night Cruising <2586447362@qq.com>
2023-11-30 23:13:40 +0800

    Day -1: Get your winter boots on!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use rocket::{get, routes};
+use rocket::get;
+use rocket::http::Status;
+use rocket::routes;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/-1/error")]
+fn internal_server_error() -> Status {
+    Status::InternalServerError
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![internal_server_error]);
 
     Ok(rocket.into())
 }
