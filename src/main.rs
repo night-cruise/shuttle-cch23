3061d66a749550fc9d33ea7a8688cdba324e8792
Night Cruising <2586447362@qq.com>
2023-12-01 22:39:45 +0800

    Day 1: Packet 'exclusive-cube' not found

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ use rocket::get;
 use rocket::http::Status;
 use rocket::routes;
 
+use std::path::PathBuf;
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![internal_server_error])
+        .mount("/", routes![cube_the_bits]);
+
+    Ok(rocket.into())
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -12,11 +24,12 @@ fn internal_server_error() -> Status {
     Status::InternalServerError
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build()
-        .mount("/", routes![index])
-        .mount("/", routes![internal_server_error]);
+#[get("/1/<path..>")]
+fn cube_the_bits(path: PathBuf) -> String {
+    let res = path
+        .iter()
+        .map(|item| item.to_str().unwrap().parse::<i32>().unwrap())
+        .fold(0, |acc, item| acc ^ item);
 
-    Ok(rocket.into())
+    (res.pow(3)).to_string()
 }
