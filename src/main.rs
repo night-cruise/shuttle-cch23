e3d8efdad12d3c9d2a7cac426f84263f71065482
Night Cruising <2586447362@qq.com>
2023-12-05 01:49:29 +0800

    Day 4: What do you call a serialized reindeer? Serdeer!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use rocket::get;
+#![allow(unused)]
+
+use rocket::{get, post, serde};
 use rocket::http::Status;
 use rocket::routes;
+use rocket::serde::{Serialize, Deserialize};
+use rocket::serde::json::{Json, Value, json};
 
 use std::path::PathBuf;
 
@@ -8,9 +12,8 @@ use std::path::PathBuf;
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
         .mount("/", routes![index])
-        .mount("/", routes![internal_server_error])
-        .mount("/", routes![cube_the_bits]);
-
+        .mount("/", routes![internal_server_error, cube_the_bits])
+        .mount("/", routes![reindeer_cheer, cursed_candy_eating_contest]);
     Ok(rocket.into())
 }
 
@@ -32,4 +35,67 @@ fn cube_the_bits(path: PathBuf) -> String {
         .fold(0, |acc, item| acc ^ item);
 
     (res.pow(3)).to_string()
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Reindeer {
+    name: String,
+    strength: i32,
+    #[serde(default)]
+    speed: f32,
+    #[serde(default)]
+    height: i32,
+    #[serde(default)]
+    antler_width: i32,
+    #[serde(default)]
+    snow_magic_power: i32,
+    #[serde(default)]
+    favorite_food: String,
+    #[serde(default)]
+    #[serde(rename="cAnD13s_3ATeN-yesT3rdAy")]
+    c_an_d13s_3_a_te_n_yes_t3rd_ay: i32
+}
+
+#[post("/4/strength", data = "<reindeers>")]
+fn reindeer_cheer(reindeers: Json<Vec<Reindeer>>) -> String {
+    let res = reindeers
+        .iter()
+        .fold(0, |acc, item| acc + item.strength);
+
+    res.to_string()
+}
+
+#[post("/4/contest", data = "<reindeers>")] 
+fn cursed_candy_eating_contest(reindeers: Json<Vec<Reindeer>>) -> Value {
+    let fastest = reindeers
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, item)| if reindeers[acc].speed < item.speed { i } else { acc });
+    let fastest_value = format!("Speeding past the finish line with a strength of {} is {}", reindeers[fastest].strength, reindeers[fastest].name);
+
+    let tallest = reindeers
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, item)| if reindeers[acc].height < item.height { i } else { acc } );
+    let tallest_value = format!("{} is standing tall with his {} cm wide antlers", reindeers[tallest].name, reindeers[tallest].antler_width);
+
+    let magician = reindeers
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, item)| if reindeers[acc].snow_magic_power < item.snow_magic_power { i } else { acc });
+    let magician_value = format!("{} could blast you away with a snow magic power of {}", reindeers[magician].name, reindeers[magician].snow_magic_power);
+
+    let consumer = reindeers
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, item)| if reindeers[acc].c_an_d13s_3_a_te_n_yes_t3rd_ay < item.c_an_d13s_3_a_te_n_yes_t3rd_ay { i } else { acc });
+    let consumer_value = format!("{} ate lots of candies, but also some {}", reindeers[consumer].name, reindeers[consumer].favorite_food);
+
+    json!({
+        "fastest": fastest_value,
+        "tallest": tallest_value,
+        "magician": magician_value,
+        "consumer": consumer_value
+    })
 }
